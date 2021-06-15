71ed44717f96317fb622e53c9f0c2ec25c010c4f
sunho1999 <wmfrlek11@naver.com>
2021-06-15 19:29:47 +0900

    데이터 정제와 트레이닝데이터만들기

---
+++ SCTF2017-Writeups/ASM/src/test.py
@@ -0,0 +1,14 @@
+import cv2
+import utils
+
+
+image = cv2.imread('img.png', cv2.IMREAD_COLOR)
+blue = utils.get_chars(image.copy(), utils.BLUE)
+green = utils.get_chars(image.copy(), utils.GREEN)
+red = utils.get_chars(image.copy(), utils.RED)
+cv2.imshow('Image Gray', blue)
+cv2.waitKey(0)
+cv2.imshow('Image Gray', green)
+cv2.waitKey(0)
+cv2.imshow('Image Gray', red)
+cv2.waitKey(0)
