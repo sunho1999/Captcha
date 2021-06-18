f8609b0b9213664f6b8a10551e1fa9ba65d862ae
sunho1999 <wmfrlek11@naver.com>
2021-06-18 21:31:50 +0900

    Captcha Hacking 자동화

--- SCTF2017-Writeups/ASM/src/run1.py
+++ SCTF2017-Writeups/ASM/src/run1.py
@@ -1,12 +1,18 @@
 import numpy as np
 import cv2
 import utils
+import requests
+import time
+import shutil
+
 
 FILE_NAME = "trained.npz"
+
 # 각 글자의 (1 x 400) 데이터와 정답 (0 ~ 9, +, *)
 with np.load(FILE_NAME) as data:
     train = data['train']
     train_labels = data['train_labels']
+
 knn = cv2.ml.KNearest_create()
 knn.train(train, cv2.ml.ROW_SAMPLE, train_labels)
 
@@ -35,4 +41,37 @@ def get_result(file_name):
         result_string += matched
     return result_string
 
-print(get_result("img_4.png"))
+host = "http://172.30.1.46:10000"
+url = '/start'
+# target_images 라는 폴더 생성
+with requests.Session() as s:
+    answer = ''
+    for i in range(0, 100):
+        start_time = time.time()
+        params = {'ans': answer}
+
+        # 정답을 파라미터에 달아서 전송하여, 이미지 경로를 받아옵니다.
+        response = s.post(host + url, params)
+        print('Server Return: ' + response.text)
+        if i == 0:
+            returned = response.text
+            image_url = host + returned
+            url = '/check'
+        else:
+            returned = response.json()
+            image_url = host + returned['url']
+        print('Problem ' + str(i) + ': ' + image_url)
+        # 특정한 폴더에 이미지 파일을 다운로드 받습니다.
+        response = s.get(image_url, stream=True)
+        target_image = './target_images/' + str(i) + '.png'
+        with open(target_image, 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        del response
+
+        # 다운로드 받은 이미지 파일을 분석하여 답을 도출합니다.
+        answer_string = get_result(target_image)
+        print('String: ' + answer_string)
+        answer_string = utils.remove_first_0(answer_string)
+        answer = str(eval(answer_string))
+        print('Answer: ' + answer)
+        print("-®-- %s seconds ---" % (time.time() - start_time))
--- SCTF2017-Writeups/ASM/src/utils.py
+++ SCTF2017-Writeups/ASM/src/utils.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import re
 
 BLUE = 0
 GREEN = 1
@@ -31,14 +32,15 @@ def extract_chars(image):
         ret, thresh = cv2.threshold(image_gray, 127, 255, 0)
         # RETR_EXTERNAL 옵션으로 숫자의 외각을 기준으로 분리
         contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
         for contour in contours:
-        # 추출된 이미지 크기가 50 이상인 경우만 실제 문자 데이터인 것으로 파악
+            # 추출된 이미지 크기가 50 이상인 경우만 실제 문자 데이터인 것으로 파악
             area = cv2.contourArea(contour)
             if area > 50:
                 x, y, width, height = cv2.boundingRect(contour)
                 roi = image_gray[y:y + height, x:x + width]
                 chars.append((x, roi))
-    chars =sorted(chars, key=lambda char: char[0])
+    chars = sorted(chars, key=lambda char: char[0])
     return chars
 
 
@@ -47,3 +49,22 @@ def resize20(image):
     resized = cv2.resize(image, (20, 20))
     return resized.reshape(-1, 400).astype(np.float32)
 
+def remove_first_0(string):
+    temp =[]
+    for i in string:
+        if i == '+' or i == '-' or i == '*':
+            temp.append(i)
+    split = re.split('\*|\+|-', string)
+    i= 0
+    temp_count =0
+    result = ""
+    for a in split:
+        a = a.lstrip('0')
+        if a == '':
+            a = '0'
+        result += a
+        if i < len(split) - 1:
+            result += temp[temp_count]
+            temp_count = temp_count + 1
+        i = i + 1
+    return result
